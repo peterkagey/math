bb6f238e20a9e8f5c866d71ebe10fdfcd775ca81
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-06-30 10:13:28 -0700

    minor refactor & special case for perfect squares.

--- graham_count/simplified_script.rb
+++ graham_count/simplified_script.rb
@@ -2,14 +2,16 @@ require_relative '../OEIS/scripts/a248663'
 require_relative '../OEIS/scripts/a006255'
 class Array
 
+  def each_subset(&block)
+    (0...2**length).each { |i| yield(subset(i)) }
+  end
+
+  private
+
   def subset(index)
     sub_array = []
     (0...index.bit_length).each { |j| sub_array << self[j] if index[j] == 1 }
     sub_array
-  end
-
-  def each_subset(&block)
-    (0...2**length).each { |i| yield(subset(i)) }
   end
 
 end
@@ -17,12 +19,17 @@ end
 def a248663(n); OEIS.a248663(n) end
 def g(n);       OEIS.a006255(n) end
 
+def middle_terms(n)
+  all_terms = (n+1...g(n)).to_a
+  n < 4 ? all_terms : all_terms.select { |i| !Prime.prime?(i) }
+end
+
 def a(n)
+  return 1 if n == g(n)
   counter = 0
-  set = (n+1...g(n)).to_a
   target = a248663(n) ^ a248663(g(n))
 
-  set.each_subset do |subset|
+  middle_terms(n).each_subset do |subset|
     counter += 1 if subset.map { |a_i| a248663(a_i) }.reduce(:^) == target
   end
   counter
