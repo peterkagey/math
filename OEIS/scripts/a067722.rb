d81c9c5ac0dc393b528c759ab24a9c9c56424d16
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-06-29 17:33:05 -0700

    comments with sequence definitions

--- OEIS/scripts/a067722.rb
+++ OEIS/scripts/a067722.rb
@@ -1,8 +1,9 @@
 require_relative 'a072905'
 
 class OEIS
+
+  # Least positive integer k such that n*(n + k) is a perfect square.
   def self.a067722(n)
-    # Least positive integer k such that n*(n + k) is a perfect square.
     return 1 if n == 0
     a072905(n) - n
   end
--- OEIS/scripts/helpers/download_hash.rb
+++ OEIS/scripts/helpers/download_hash.rb
@@ -6,9 +6,11 @@ def web_contents(id)
   open(url) { |f| f.read }
 end
 
-def text_to_hash(id)
+# A000040 is the prime numbers
+# seq_to_hash(40)
+# >> {0=>2, 1=>3, 2=>5, ... }
+def seq_to_hash(id)
   text = web_contents(id)
   rows = text.split("\n").reject { |str| str[0] == "#" }
   Hash[rows.collect { |str| str.split(" ").map(&:to_i) }]
 end
-
