ab137a75dccbf37c89607abd5ce617eeb0b3d71e
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-07-01 17:26:10 -0700

    make spec_builder more object oriented

--- OEIS/scripts/helpers/download_hash.rb
+++ OEIS/scripts/helpers/download_hash.rb
@@ -1,7 +1,7 @@
 require 'open-uri'
 
 def web_contents(id)
-  id = "#{id}".rjust(6, '0')
+  id = "#{id}"[/\d+/].rjust(6, '0')
   url = "https://oeis.org/A#{id}/b#{id}.txt"
   open(url) { |f| f.read }
 end
@@ -12,5 +12,6 @@ end
 def seq_to_hash(id)
   text = web_contents(id)
   rows = text.split("\n").reject { |str| str[0] == "#" }
+  raise "No b-file for #{id}!" if rows == []
   Hash[rows.collect { |str| str.split(" ").map(&:to_i) }]
 end
--- OEIS/scripts/helpers/spec_builder.rb
+++ OEIS/scripts/helpers/spec_builder.rb
@@ -1,34 +1,66 @@
 require_relative "download_hash"
 
-project_root = File.dirname(__FILE__)
-Dir.glob(project_root + '/../*.rb').each do |file|
-  next unless name = file[/a\d{6}/]
-  file_name = project_root + "/../../specs/#{name}_spec.rb"
-  next if File.exist?(file_name)
-  hash = text_to_hash(name[1..-1])
-  min = hash.keys.min
-  r = (min..min+4).to_a
+class TestBuilder
 
-  str = %(require_relative '../scripts/#{name}'
+  def initialize(sequence_id)
+    @id = "a" + sequence_id.to_s[/\d+/].rjust(6, '0')
+    write_test unless test_already_exists?
+  end
+
+  def test_already_exists?
+    if File.exist?(spec_file_path)
+      puts "Test already exists for #{@id}!"
+      true
+    else
+      false
+    end
+  end
+
+  def helpers_directory_root
+    File.dirname(__FILE__)
+  end
+
+  def spec_file_path
+    helpers_directory_root + "/../../specs/#{@id}_spec.rb"
+  end
+
+  def sequence_hash
+    @sequence_hash ||= seq_to_hash(@sequence_id)
+  end
+
+  def minimum_argument
+    sequence_hash.keys.min
+  end
+
+  def r # range (first five values)
+    (minimum_argument...minimum_argument+5).to_a
+  end
+
+  def test
+    %(require_relative '../scripts/#{@id}'
 
 describe OEIS do
 
   def a(n)
-    OEIS.#{name}(n)
+    OEIS.#{@id}(n)
   end
 
   it "should know first five values" do
-    expect(a(#{r[0]})).to eq #{hash[r[0]]}
-    expect(a(#{r[1]})).to eq #{hash[r[1]]}
-    expect(a(#{r[2]})).to eq #{hash[r[2]]}
-    expect(a(#{r[3]})).to eq #{hash[r[3]]}
-    expect(a(#{r[4]})).to eq #{hash[r[4]]}
+    expect(a(#{r[0]})).to eq #{sequence_hash[r[0]]}
+    expect(a(#{r[1]})).to eq #{sequence_hash[r[1]]}
+    expect(a(#{r[2]})).to eq #{sequence_hash[r[2]]}
+    expect(a(#{r[3]})).to eq #{sequence_hash[r[3]]}
+    expect(a(#{r[4]})).to eq #{sequence_hash[r[4]]}
   end
 
 end
 )
+  end
 
-  File.write(file_name, str)
+  def write_test
+    File.write(spec_file_path, test)
+  end
+
 end
 
-
+TestBuilder.new(72905)
