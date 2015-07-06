4a9fbffbfc69127f17c2580483c90ca798ec96f5
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-07-06 12:13:46 -0700

    Code golf.

--- OEIS/scripts/helpers/palindrome_counter.rb
+++ OEIS/scripts/helpers/palindrome_counter.rb
@@ -1,13 +1,9 @@
 def palindrome_count(n, b)
-
   seq = [0, 0]
-  i = 2
-  loop do
-    return seq[n] if seq[n]
-    k = (i+1)/2
-    a_i = b * seq[i-1] + b**k - seq[k]
-    seq << a_i
-    i += 1
+  i = 0
+  while seq[n].nil? && i += 1
+    k = 1 + i/2
+    seq << b * seq[i] + b**k - seq[k]
   end
-
+  seq[n]
 end
