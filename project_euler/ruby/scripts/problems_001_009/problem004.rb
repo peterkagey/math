24354605c3aa6bd3c4ef696f33b18f0a9b1323a2
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-07-17 13:20:52 -0700

    Problem 4 spec.

--- project_euler/ruby/scripts/problems_001_009/problem004.rb
+++ project_euler/ruby/scripts/problems_001_009/problem004.rb
@@ -1,21 +1,19 @@
-# A palindromic number reads the same both ways. The largest palindrome made
-# from the product of two 2-digit numbers is 9009 = 91 × 99.
+class ProjectEuler
+  # A palindromic number reads the same both ways. The largest palindrome made
+  # from the product of two 2-digit numbers is 9009 = 91 × 99.
 
-# Find the largest palindrome made from the product of two 3-digit numbers.
-
-start = Time.now
-
-c = 0
-999.downto(100).each do |a|
-	k = [c/a, a].max
-	(k...999).each do |b|
-		str = (a*b).to_s
-		c = a * b if str == str.reverse
-	end
+  # Find the largest palindrome made from the product of two 3-digit numbers.
+  def self.problem004
+    c = 0
+    999.downto(100).each do |a|
+    	k = [c/a, a].max
+    	(k...999).each do |b|
+    		str = (a*b).to_s
+    		c = a * b if str == str.reverse
+    	end
+    end
+    c
+  end
+  # 906609
+  # 0.002313 seconds
 end
-
-p c
-p Time.now - start
-
-# 906609
-# 0.002313 seconds
---
+++ project_euler/ruby/specs/problems_001_009/problem004_spec.rb
@@ -0,0 +1,8 @@
+require '/Users/pkagey/personal/math/project_euler/ruby/scripts/problems_001_009/problem004.rb'
+describe ProjectEuler do
+
+  it "should compute correctly" do
+    expect(ProjectEuler.problem004).to eq 906609
+  end
+
+end
