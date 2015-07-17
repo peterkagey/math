f121ff35350ab81f3fb0bc6da7aad8919301371d
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-07-17 13:23:05 -0700

    Problem 5 spec.

--- project_euler/ruby/scripts/problems_001_009/problem005.rb
+++ project_euler/ruby/scripts/problems_001_009/problem005.rb
@@ -1,16 +1,14 @@
-# 2520 is the smallest number that can be divided by each of the numbers from
-# 1 to 10 without any remainder.
+class ProjectEuler
+  # 2520 is the smallest number that can be divided by each of the numbers from
+  # 1 to 10 without any remainder.
 
-# What is the smallest positive number that is evenly divisible by all of the
-# numbers from 1 to 20?
-
-start = Time.now
-
-product = 1
-(1..20).each{ |n| product = product.lcm(n) }
-
-p product
-p Time.now - start
-
-# 232792560
-# 1.4e-05 seconds
+  # What is the smallest positive number that is evenly divisible by all of the
+  # numbers from 1 to 20?
+  def self.problem005
+    product = 1
+    (1..20).each{ |n| product = product.lcm(n) }
+    product
+  end
+  # 232792560
+  # 1.4e-05 seconds
+end
---
+++ project_euler/ruby/specs/problems_001_009/problem005_spec.rb
@@ -0,0 +1,8 @@
+require '/Users/pkagey/personal/math/project_euler/ruby/scripts/problems_001_009/problem005.rb'
+describe ProjectEuler do
+
+  it "should compute correctly" do
+    expect(ProjectEuler.problem005).to eq 232792560
+  end
+
+end
