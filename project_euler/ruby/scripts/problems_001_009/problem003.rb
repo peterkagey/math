cd106566967bfe0d669f80578cf427654e96e1e7
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-07-17 13:19:08 -0700

    Problem 3 spec.

--- project_euler/ruby/scripts/problems_001_009/problem003.rb
+++ project_euler/ruby/scripts/problems_001_009/problem003.rb
@@ -1,18 +1,16 @@
-# The prime factors of 13195 are 5, 7, 13 and 29.
+require 'prime'
 
-# What is the largest prime factor of the number 600851475143 ?
+class ProjectEuler
 
-start = Time.now
-require_relative '../function/sieve_of_eratosthenes'
-
-def problem003(a)
-  Prime.each(6900) do |prime|
-    a /= prime while a % prime == 0
-    return prime if a == 1
+  # The prime factors of 13195 are 5, 7, 13 and 29.
+  # What is the largest prime factor of the number 600851475143 ?
+  def self.problem003
+    a = 600851475143
+    Prime.each(6900) do |prime|
+      a /= prime while a % prime == 0
+      return prime if a == 1
+    end
   end
+  # 6857
+  # 0.003549 seconds
 end
-
-p problem003(600851475143)
-p Time.now-start
-# 6857
-# 0.003549 seconds
---
+++ project_euler/ruby/specs/problems_001_009/problem003_spec.rb
@@ -0,0 +1,8 @@
+require '/Users/pkagey/personal/math/project_euler/ruby/scripts/problems_001_009/problem003.rb'
+describe ProjectEuler do
+
+  it "should compute correctly" do
+    expect(ProjectEuler.problem003).to eq 6857
+  end
+
+end
