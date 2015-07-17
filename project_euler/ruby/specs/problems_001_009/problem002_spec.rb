48c80120b5d1d8067801f768ec6c8da879a89432
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-07-17 13:16:05 -0700

    Problem 2 spec

---
+++ project_euler/ruby/specs/problems_001_009/problem002_spec.rb
@@ -0,0 +1,8 @@
+require '/Users/pkagey/personal/math/project_euler/ruby/scripts/problems_001_009/problem002.rb'
+describe ProjectEuler do
+
+  it "should compute correctly" do
+    expect(ProjectEuler.problem002).to eq 4613732
+  end
+
+end
