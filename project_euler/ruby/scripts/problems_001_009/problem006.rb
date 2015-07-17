26ba7dd3a59fe5f7095bad787d00b1ed0e6cc964
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-07-17 13:25:37 -0700

    Problem 6 spec.

--- project_euler/ruby/scripts/problems_001_009/problem006.rb
+++ project_euler/ruby/scripts/problems_001_009/problem006.rb
@@ -1,23 +1,23 @@
-# The sum of the squares of the first ten natural numbers is,
+require '/Users/pkagey/personal/math/project_euler/ruby/helpers/triangle.rb'
+class ProjectEuler
+  # The sum of the squares of the first ten natural numbers is,
 
-# 1^2 + 2^2 + ... + 10^2 = 385
-# The square of the sum of the first ten natural numbers is,
+  # 1^2 + 2^2 + ... + 10^2 = 385
+  # The square of the sum of the first ten natural numbers is,
 
-# (1 + 2 + ... + 10)^2 = 552 = 3025
-# Hence the difference between the sum of the squares of the first ten
-# natural numbers and the square of the sum is 3025 − 385 = 2640.
+  # (1 + 2 + ... + 10)^2 = 552 = 3025
+  # Hence the difference between the sum of the squares of the first ten
+  # natural numbers and the square of the sum is 3025 − 385 = 2640.
 
-# Find the difference between the sum of the squares of the first one hundred
-# natural numbers and the square of the sum.
+  # Find the difference between the sum of the squares of the first one hundred
+  # natural numbers and the square of the sum.
+  def self.problem006
+    n = 100
+    sum_to_n_squared = (n * (n + 1) / 2)**2         # = (1 + 2 + ... + n)^2
+    sum_of_squares   = n * (n + 1) * (2 * n + 1)/6  # = 1^2 + 2^2 + ... + n^2
 
-start = Time.now
-
-n = 100
-sum_to_n_squared = (n * (n + 1) / 2)**2         # = (1 + 2 + ... + n)^2
-sum_of_squares   = n * (n + 1) * (2 * n + 1)/6  # = 1^2 + 2^2 + ... + n^2
-
-p sum_to_n_squared - sum_of_squares
-p Time.now - start
-
-# 25164150
-# 2.5e-05 seconds
+    sum_to_n_squared - sum_of_squares
+  end
+  # 25164150
+  # 2.5e-05 seconds
+end
---
+++ project_euler/ruby/specs/problems_001_009/problem006_spec.rb
@@ -0,0 +1,8 @@
+require '/Users/pkagey/personal/math/project_euler/ruby/scripts/problems_001_009/problem006.rb'
+describe ProjectEuler do
+
+  it "should compute correctly" do
+    expect(ProjectEuler.problem006).to eq 25164150
+  end
+
+end
