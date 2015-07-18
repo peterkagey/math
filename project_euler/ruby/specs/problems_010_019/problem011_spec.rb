404effde3a37eee160403ef7ad165443344dede4
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-07-17 17:24:19 -0700

    Problem 11 spec.

---
+++ project_euler/ruby/specs/problems_010_019/problem011_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../../scripts/problems_010_019/problem011.rb'
+describe ProjectEuler do
+
+  it "should compute correctly" do
+    expect(ProjectEuler.problem011).to eq 70600674
+  end
+
+end
