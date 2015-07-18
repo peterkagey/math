5915156a96876e5f3c0f03a7a0baf5fc47769cd6
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-07-17 17:09:13 -0700

    Problem 7 spec.

--- project_euler/ruby/scripts/problems_001_009/problem007.rb
+++ project_euler/ruby/scripts/problems_001_009/problem007.rb
@@ -1,20 +1,12 @@
-# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
-# that the 6th prime is 13.
-# What is the 10 001st prime number?
+require_relative '../../helpers/first_n_primes'
 
-start = Time.now
-require_relative '../function/sieve_of_eratosthenes'
+class ProjectEuler
 
-def log(n); Math.log(n) end
-
-def first_n_primes(n)
-	k = 67 if n < 20 						  	  # upper bound on primes
-	k ||= (n * (log(n) + log(log(n)) - 0.5)).ceil # is valid for n >= 20
-	sieve_of_eratosthenes(k)[0...n]
+  # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+  # that the 6th prime is 13.
+  # What is the 10 001st prime number?
+  def self.problem007
+    first_n_primes(10001).last
+  end
+  # 0.024731 seconds
 end
-
-p first_n_primes(10001)[-1]
-p Time.now - start
-
-# 104743
-# 0.024731 seconds
---
+++ project_euler/ruby/specs/problems_001_009/problem007_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../../scripts/problems_001_009/problem007.rb'
+describe ProjectEuler do
+
+  it "should compute correctly" do
+    expect(ProjectEuler.problem007).to eq 104743
+  end
+
+end
