5f26c55aa2b1cff2f4dadd07c6c8ac1742e6cecb
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-07-17 17:19:13 -0700

    Problem 9 spec.

--- project_euler/ruby/scripts/problems_001_009/problem009.rb
+++ project_euler/ruby/scripts/problems_001_009/problem009.rb
@@ -7,21 +7,16 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
-start = Time.now
-
-def problem_9
-	(334...500).each do |c|
-		(0..c).each do |b|
-			a = Math.sqrt(c**2 - b**2)
-			next unless a == a.to_i
-      return [a,b,c] if a + b + c == 1000
-			return (a*b*c).to_i if a + b + c == 1000
-		end
-	end
+class ProjectEuler
+  def self.problem009
+  	(334...500).each do |c|
+  		(0..c).each do |b|
+  			a = Math.sqrt(c**2 - b**2)
+  			next unless a == a.to_i
+  			return (a*b*c).to_i if a + b + c == 1000
+  		end
+  	end
+  end
 end
-
-p problem_9
-p Time.now-start
-
 # 31875000
 # 0.016549 seconds
---
+++ project_euler/ruby/specs/problems_001_009/problem009_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../../scripts/problems_001_009/problem009.rb'
+describe ProjectEuler do
+
+  it "should compute correctly" do
+    expect(ProjectEuler.problem009).to eq 31875000
+  end
+
+end
