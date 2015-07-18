f33f94c7482dc82e229a7531ee6a4a0b9ccb4d9a
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-07-17 17:21:30 -0700

    Problem 10 spec.

--- project_euler/ruby/scripts/problems_010_019/problem010.rb
+++ project_euler/ruby/scripts/problems_010_019/problem010.rb
@@ -1,11 +1,13 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all of the primes below two million.
 
-start = Time.now
 require 'prime'
 
-p Prime.each(2_000_000).reduce(:+)
-p Time.now - start
+class ProjectEuler
+  def self.problem010
+    Prime.each(2_000_000).reduce(:+)
+  end
+end
 
 # 142913828922
 # 0.471795 seconds
