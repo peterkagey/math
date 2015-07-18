c15f5e6e4331e2d94dfa812573cc3c368d9eba86
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-07-17 17:16:51 -0700

    Problem 8 spec.

---
+++ project_euler/ruby/specs/problems_001_009/problem008_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../../scripts/problems_001_009/problem008.rb'
+describe ProjectEuler do
+
+  it "should compute correctly" do
+    expect(ProjectEuler.problem008).to eq 23514624000
+  end
+
+end
