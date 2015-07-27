65c970e37e8bd498724996612fa1948986a95eeb
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-07-27 13:44:23 -0700

    Refactor A259527 script to exploit A260510.

--- OEIS/ruby/scripts/graham/a259527.rb
+++ OEIS/ruby/scripts/graham/a259527.rb
@@ -1,13 +1,10 @@
-require_relative 'a006255'
-require_relative 'a248663'
-require_relative '../helpers/perfect_square_product'
+require_relative 'a260510'
 
 class OEIS
   # a(n) counts the number of sequences n = b_1 < b_2 < ... < b_t = A006255(n)
   # such that b_1*b_2*...*b_t is a perfect square.
   def self.a259527(n)
-    ary = (n..a006255(n)).to_a.map{ |i| a248663(i) }
-    2**(BooleanMatrix.new(ary).dim_null_space - 1)
+    2**a260510(n)
   end
 
 end
