03edb6ba25795fa1422c4916d6422c287f574167
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-09-11 14:41:25 -0700

    A259439 with test and b-file.

---
+++ OEIS/ruby/specs/miscellaneous/a259439_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../../scripts/miscellaneous/a259439'
+
+describe OEIS do
+
+  def a(n)
+    OEIS.a259439(n)
+  end
+
+  it "should know first ten values" do
+    expect(a(1)).to eq 1
+    expect(a(2)).to eq 1
+    expect(a(3)).to eq 1
+    expect(a(4)).to eq 1
+    expect(a(5)).to eq 1
+    expect(a(6)).to eq 2
+    expect(a(7)).to eq 1
+    expect(a(8)).to eq 2
+    expect(a(9)).to eq 3
+    expect(a(10)).to eq 5
+  end
+
+end
