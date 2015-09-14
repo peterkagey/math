dd23a93bc634a2599ed37ca3be367ec740cea456
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-09-14 10:02:15 -0700

    A098164 with test and b-file.

---
+++ OEIS/ruby/specs/miscellaneous/a098164_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../../scripts/miscellaneous/a098164'
+
+describe OEIS do
+
+  def a(n)
+    OEIS.a098164(n)
+  end
+
+  it "should handle small values" do
+    expect(a(0)).to eq 0
+    expect(a(10)).to eq 9
+    expect(a(11)).to eq 20
+    expect(a(12)).to eq 11
+    expect(a(13)).to eq 22
+    expect(a(51)).to eq 201
+    expect(a(53)).to eq 23
+    expect(a(54)).to eq 12
+  end
+
+end
