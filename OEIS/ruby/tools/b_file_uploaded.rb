c09e97ab3285a888db2f15e210f4cde7fe37922b
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-09-15 10:22:33 -0700

    Minor refactor of b_file_uploaded.rb

--- OEIS/ruby/tools/b_file_uploaded.rb
+++ OEIS/ruby/tools/b_file_uploaded.rb
@@ -4,15 +4,15 @@ require_relative 'helpers/sequence_path_iterator'
 class BFileCompare
 
   PENDING_SEQUENCES = {
+    "A019555" => "Awaiting upload.",
     "A047932" => "Awaiting upload.",
     "A053797" => "Awaiting upload.",
+    "A072905" => "Awaiting upload.",
     "A098164" => "Awaiting upload.",
     "A121341" => "Awaiting upload.",
     "A143051" => "Awaiting upload.",
     "A163325" => "Awaiting upload.",
     "A258448" => "Awaiting upload.",
-    "A019555" => "Awaiting upload.",
-    "A072905" => "Awaiting upload.",
 
     "A143480" => "Draft (2015/09/03).",
     "A143481" => "Draft (2015/09/03).",
@@ -24,15 +24,16 @@ class BFileCompare
   }
 
   attr_reader :local_range, :oeis_range
+
   def initialize(sequence_id)
     @id = sequence_id
-    @local_range = known_b_file_range
-    @oeis_range = active_b_file_range
+    @local_range = local_range
+    @oeis_range = oeis_range
   end
 
   def skip?
-    return false if File.exists? BFilePathIterator.number_to_path(@id)
-    true
+    return true if no_local_b_file?
+    false
   end
 
   def pending?
@@ -43,19 +44,22 @@ class BFileCompare
     PENDING_SEQUENCES.find { |name, _| name =~ /#{@id}/}.last rescue "Unknown!"
   end
 
-  private
-
-  def active_b_file_range
+  def internal_format
     base = "https://oeis.org/search?q=id:A"
     format = "&fmt=text"
-    internal_format = open("#{base}#{@id}#{format}") { |f| f.read }.split("\n")
-    b_file_descriptor = internal_format.find { |line| line =~ /^%H.+\d+\.\.\d+/ }
+    open("#{base}#{@id}#{format}") { |f| f.read }.split("\n")
+  end
+
+  def oeis_range
+    range_regex = /^%H.+\d+\.\.\d+/
+    b_file_descriptor = internal_format.find { |line| line =~ range_regex }
     return "missing" if b_file_descriptor.nil?
+
     desc, min, max = b_file_descriptor.match(/n = (\d+)\.\.(\d+)/).to_a
     "#{min}..#{max}"
   end
 
-  def known_b_file_range
+  def local_range
     b_file_path = BFilePathIterator.number_to_path(@id)
     return ".." if !File.exists? b_file_path
     pairs = File.read(b_file_path).split(/\s/).each_slice(2).map(&:first)
