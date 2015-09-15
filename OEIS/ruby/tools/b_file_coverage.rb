0407b8b6417d032284c1c09c70b9cf3e3f2d665a
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-09-15 10:10:43 -0700

    Order sequences lexicographically.

--- OEIS/ruby/tools/b_file_coverage.rb
+++ OEIS/ruby/tools/b_file_coverage.rb
@@ -8,9 +8,9 @@ class BFileCoverage
   EXPECTED = [
     "b000002", # a(n) is length of n-th run
     "b000073", # Tribonacci numbers
+    "b000188", # Square root of largest square dividing n.
     "b005117", # Squarefree numbers
     "b007913", # Squarefree part of n
     "b070229", # Next m>n such that m is divisible by lpf(n)
-    "b000188", # Square root of largest square dividing n.
   ]
 end
