92bd0096ccb1329b08241e6552da1a779719661a
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-09-14 21:17:34 -0700

    Move A005117_spec.rb to analogous file path of script.

--- OEIS/ruby/specs/miscellaneous/a005117_spec.rb
+++ OEIS/ruby/specs/miscellaneous/a005117_spec.rb
@@ -6,14 +6,12 @@ describe OEIS do
     OEIS.a005117(n)
   end
 
-  it "should handle small values" do
+  it "should know first five values" do
     expect(a(1)).to eq 1
     expect(a(2)).to eq 2
     expect(a(3)).to eq 3
     expect(a(4)).to eq 5
     expect(a(5)).to eq 6
-    expect(a(6)).to eq 7
-    expect(a(7)).to eq 10
   end
 
 end
