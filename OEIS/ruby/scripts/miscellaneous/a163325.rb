47ab7a82910187167390b8c3fdbb897a5d2ba001
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-09-14 21:28:19 -0700

    A163325 fix with test.

--- OEIS/ruby/scripts/miscellaneous/a163325.rb
+++ OEIS/ruby/scripts/miscellaneous/a163325.rb
@@ -1,7 +1,7 @@
 class OEIS
   def self.a163325(n)
-    base_3_ary = n.to_s(3).split('')
+    base_3_ary = n.to_s(3).split('').reverse
     base_3_ary = base_3_ary.each_with_index.select { |x, i| i.even? }.map(&:first)
-    base_3_ary.join.to_i(3)
+    base_3_ary.reverse.join.to_i(3)
   end
 end
---
+++ OEIS/ruby/specs/miscellaneous/a163325_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../../scripts/miscellaneous/a163325'
+
+describe OEIS do
+
+  def a(n)
+    OEIS.a163325(n)
+  end
+
+  it "should know first five values" do
+    expect(a(0)).to eq 0
+    expect(a(1)).to eq 1
+    expect(a(2)).to eq 2
+    expect(a(3)).to eq 0
+    expect(a(4)).to eq 1
+  end
+
+end
