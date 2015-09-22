c2f930379c38c2b3f74f9be3d3fcec30a0054bc7
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-09-22 15:25:58 -0700

    Increase upper bound.

--- OEIS/ruby/scripts/helpers/is_square.rb
+++ OEIS/ruby/scripts/helpers/is_square.rb
@@ -1,11 +1,10 @@
 class SquareLookup
 
-  MAX = 10000
+  MAX = 40_000
 
   def self.squarefree_numbers
     squares = (2..MAX**0.5).to_a.map { |i| i**2 }
-    bool_ary = Array.new(10001) { true }
-    bool_ary[0] = false
+    bool_ary = Array.new(MAX + 1) { true }
     squares.each do |square|
       (1..MAX/square).count { |i| bool_ary[square * i] = false}
     end
@@ -13,7 +12,7 @@ class SquareLookup
   end
 
   def self.squares
-    bool_ary = Array.new(10001) { false }
+    bool_ary = Array.new(MAX + 1) { false }
     squares = (0..MAX**0.5).each { |i| bool_ary[i**2] = true }
     bool_ary
   end
@@ -24,9 +23,10 @@ class Integer
 
   SQUAREFREE = SquareLookup.squarefree_numbers
   SQUARE = SquareLookup.squares
+  UPPER_BOUND = SquareLookup::MAX
 
   def is_square?
-    return SQUARE[self] if self <= 10_000 && self >= 0
+    return SQUARE[self] if self <= UPPER_BOUND && self >= 0
     k = (self**0.5).round
     k**2 == self
   end
@@ -36,13 +36,13 @@ class Integer
   end
 
   def is_squarefree?
-    return SQUAREFREE[self] if self <= 10_000 && self >= 0
-    raise "Input is out of range! (#{n} is not in (0..10_000)"
+    return SQUAREFREE[self] if self <= UPPER_BOUND && self >= 0
+    raise "Input is out of range! (#{n} is not in (0..UPPER_BOUND)"
   end
 
   def is_nonsquarefree?
-    return !SQUAREFREE[self] if self <= 10_000 && self >= 0
-    raise "Input is out of range! (#{n} is not in (0..10_000)"
+    return !SQUAREFREE[self] if self <= UPPER_BOUND && self >= 0
+    raise "Input is out of range! (#{n} is not in (0..UPPER_BOUND)"
   end
 
 end
