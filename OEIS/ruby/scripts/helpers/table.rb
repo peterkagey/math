5dca960b0bd11d3d69e379af075a767131ea1090
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-09-30 13:02:02 -0700

    Update table code to start from n >= 0, k >= 0.

--- OEIS/ruby/scripts/helpers/table.rb
+++ OEIS/ruby/scripts/helpers/table.rb
@@ -4,7 +4,7 @@ class OEISTable
     coordinate_list = []
     (0..terms).each do |i|
       n, k = n_k(i)
-      coordinate_list << yield(n - 1, k - 1)
+      coordinate_list << yield(n, k)
     end
     coordinate_list
   end
@@ -19,11 +19,9 @@ class OEISTable
 
   def self.n_k(j)
     i = (ith_triangle_number_less_than j).to_i
-    k = j - triangle(i) + 1
-    n = i - k + 2
+    k = j - triangle(i)
+    n = i - k
     [n, k]
   end
 
 end
-
-p OEISTable.collect_coordinates(20) { |n, k| n | k }
--- OEIS/ruby/scripts/tables/a051125.rb
+++ OEIS/ruby/scripts/tables/a051125.rb
@@ -5,7 +5,7 @@ class OEIS
   def self.a051125(j)
     raise "A051125 is not defined for n = #{j}" if j < 1
     n, k = OEISTable.n_k(j - 1)
-    [n, k].max
+    [n, k].max + 1
   end
 
 end
