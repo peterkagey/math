44ca7dc7cc5cf6f2ab24b7d5640ada797b5e5905
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-10-12 09:08:37 -0700

    Refactor.

--- project_euler/ruby/helpers/prime_factors.rb
+++ project_euler/ruby/helpers/prime_factors.rb
@@ -1,12 +1,11 @@
 def prime_factors(n, primes)
   prime_factors_hash = Hash.new(0)
   primes.each do |k|
-    loop do
-      break if n % k != 0
+    break if n < k
+    while n % k == 0
       n /= k
       prime_factors_hash[k] += 1
     end
-    break if n == 1
   end
   prime_factors_hash
 end
