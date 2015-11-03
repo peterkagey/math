9116e2f37105cdecb177caedfbf924ed2df39a53
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-11-03 09:46:49 -0800

    A060875 with test and b-file.

---
+++ OEIS/ruby/scripts/intrinsic_k_palindromes/a060875.rb
@@ -0,0 +1,12 @@
+require_relative '../helpers/palindromes'
+
+class OEIS
+
+  A060875_SEQUENCE = IntrinsicKPalindromes.new(5, 10000).all
+
+  def self.a060875(n)
+    raise "A060875 is not defined for n = #{n} < 1" if n < 1
+    A060875_SEQUENCE[n - 1]
+  end
+
+end
