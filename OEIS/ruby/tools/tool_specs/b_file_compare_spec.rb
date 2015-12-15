d7de65ff2ff1ba1da5b71e2aeabf864fb73ba0bc
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-12-14 23:41:30 -0800

    Updates b-file cache and exceptions; additional b-files.

--- OEIS/ruby/tools/tool_specs/b_file_compare_spec.rb
+++ OEIS/ruby/tools/tool_specs/b_file_compare_spec.rb
@@ -2,8 +2,8 @@ require_relative '../b_file_compare'
 
 PENDING_SEQUENCES ||= {
   "A217489" => "Draft (2015/11/03).",
-  "A082647" => "Draft (2015/12/08).",
-  "A265389" => "Draft (2015/12/08)."
+  "A265389" => "Draft (2015/12/08).",
+  "A260643" => "Draft (2015/11/11)."
 }
 
 SequencePathIterator.sequence_numbers.each do |id|
