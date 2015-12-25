95a7515f4609cb271f40a55d0423d07ed707dcb3
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-12-24 16:50:17 -0800

    Clearer spec message.

--- OEIS/ruby/tools/tool_specs/b_file_coverage_spec.rb
+++ OEIS/ruby/tools/tool_specs/b_file_coverage_spec.rb
@@ -2,7 +2,7 @@ require_relative '../b_file_coverage'
 
 describe "OEIS Tests" do
 
-  it "should test every sequence script" do
+  it "should check that all b-files are present." do
     unexpectedly_missing_b_files = BFileCoverage.missing_b_files - BFileCoverage::EXPECTED
     expect(unexpectedly_missing_b_files).to eq []
   end
