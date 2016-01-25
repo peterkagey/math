455644753623afb8ba776daa5e8a54e43badfa40
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-01-25 11:09:46 -0800

    A268038 with test and b-file.

---
+++ Sloanes/lib/scripts/miscellaneous/a268038.rb
@@ -0,0 +1,15 @@
+require_relative "../helper_sequences/a000196"
+require_relative "../helper_sequences/a001057"
+require_relative "../helper_sequences/a002061"
+require_relative "../helper_sequences/a002522"
+
+class OEIS
+
+  def self.a268038(n)
+    k = ((n - 1)**0.5).ceil
+    return -a001057(k - 1) if (a002061(k)..a002522(k)).include?(n)
+    j, h = n - a002522(k - 1), a001057(k - 2)
+    k.odd? ? j - h : -(j + h)
+  end
+
+end
---
+++ Sloanes/lib/specs/miscellaneous/a268038_spec.rb
@@ -0,0 +1,38 @@
+require_relative '../../scripts/miscellaneous/a268038'
+
+describe OEIS do
+
+  def a(n)
+    OEIS.a268038(n)
+  end
+
+  it "should know first fourteen values" do
+    expect(a(1)).to eq 0
+    expect(a(2)).to eq 0
+    expect(a(3)).to eq -1
+    expect(a(4)).to eq -1
+    expect(a(5)).to eq -1
+    expect(a(6)).to eq 0
+    expect(a(7)).to eq 1
+    expect(a(8)).to eq 1
+    expect(a(9)).to eq 1
+    expect(a(10)).to eq 1
+    expect(a(11)).to eq 0
+    expect(a(12)).to eq -1
+    expect(a(13)).to eq -2
+    expect(a(14)).to eq -2
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
