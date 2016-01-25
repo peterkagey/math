c093221d2134c2f1c1a83a8883f7bffbb96ae96b
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-01-25 11:24:37 -0800

    Add b-files. Update specs.

--- Sloanes/lib/specs/remainder_game/a268060_spec.rb
+++ Sloanes/lib/specs/remainder_game/a268060_spec.rb
@@ -6,12 +6,22 @@ describe OEIS do
     OEIS.a268060(n)
   end
 
-  it "should know first five values" do
+  it "should know first 15 values" do
     expect(a(1)).to eq 1
     expect(a(2)).to eq 1
     expect(a(3)).to eq 2
     expect(a(4)).to eq 3
     expect(a(5)).to eq 3
+    expect(a(6)).to eq 4
+    expect(a(7)).to eq 4
+    expect(a(8)).to eq 5
+    expect(a(9)).to eq 5
+    expect(a(10)).to eq 6
+    expect(a(11)).to eq 7
+    expect(a(12)).to eq 7
+    expect(a(13)).to eq 8
+    expect(a(14)).to eq 9
+    expect(a(15)).to eq 8
   end
 
 end
