168ba44c802b44fd78e5093deee90ded7f7cbfbf
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-01-25 11:10:42 -0800

    A268057 with test and b-file.

---
+++ Sloanes/lib/scripts/tables/a268057.rb
@@ -0,0 +1,14 @@
+require_relative "../helpers/remainder_game"
+require_relative "../helpers/table"
+
+class OEIS
+
+  # https://www.reddit.com/r/math/comments/409dfe/does_anyone_know_anything_about_this_idea_i/
+  # Similar to A107435
+  def self.a268057(i)
+    raise "A268057 is not defined for n = #{i}" if i < 1
+    n, k = OEISTable.n_k(i - 1)
+    RemainderGame.length(n + k + 1, k + 1)
+  end
+
+end
