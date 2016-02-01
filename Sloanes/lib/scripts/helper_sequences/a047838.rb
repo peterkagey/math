c131d435f52fb8bc9a543b0fe8d5fa99b6efc7ab
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-02-01 10:40:48 -0800

    A047838 with test.

---
+++ Sloanes/lib/scripts/helper_sequences/a047838.rb
@@ -0,0 +1,7 @@
+class OEIS
+
+  def self.a047838(n)
+    n * n / 2 - 1
+  end
+
+end
---
+++ Sloanes/lib/specs/helper_sequences/a047838_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../../scripts/helper_sequences/a047838'
+
+describe OEIS do
+
+  def a(n)
+    OEIS.a047838(n)
+  end
+
+  it "should know first 10 values" do
+    expect(a(2)).to eq 1
+    expect(a(3)).to eq 3
+    expect(a(4)).to eq 7
+    expect(a(5)).to eq 11
+    expect(a(6)).to eq 17
+    expect(a(7)).to eq 23
+    expect(a(8)).to eq 31
+    expect(a(9)).to eq 39
+    expect(a(10)).to eq 49
+    expect(a(11)).to eq 59
+  end
+
+end
