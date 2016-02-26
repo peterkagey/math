54b1b3e3108c140bffc3f5b415e4a1bb93e08b3a
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-02-25 21:52:04 -0800

    Refactor A255363 to be more functional.

--- Sloanes/lib/scripts/graham/a255363.rb
+++ Sloanes/lib/scripts/graham/a255363.rb
@@ -4,10 +4,9 @@ require_relative 'a006255'
 class OEIS
   # Numbers with the property that A006255(k) = A070229(k).
   def self.a255363(n)
-    k, counter = 0, 0
-    while k += 1 do
-      counter += 1 if a070229(k) == a006255(k)
+    (0..Float::INFINITY).inject(0) do |counter, k|
       return k if counter == n
+      a070229(k + 1) == a006255(k + 1) ? counter + 1 : counter
     end
   end
 end
