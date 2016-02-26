a688dfba926ab5e63afdb048d1e615ef3f8eb7fe
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-02-25 21:51:16 -0800

    Refactor A255980 to be more functional.

--- Sloanes/lib/scripts/graham/a255980.rb
+++ Sloanes/lib/scripts/graham/a255980.rb
@@ -5,9 +5,9 @@ class OEIS
 
   # Number of iterations of A067565 required to reach a perfect square.
   def self.a255980(n)
-    c = 0
-    n = a067565(n) while n.is_nonsquare? && c += 1
-    c
+    (0..Float::INFINITY).inject(n) do |k, c|
+      k.is_nonsquare? ? a067565(k) : (return c)
+    end
   end
 
 end
