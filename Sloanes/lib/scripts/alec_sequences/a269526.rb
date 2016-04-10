0edc0c59708cb7a8460e915836a93e74e72b7d03
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-04-09 17:46:26 -0700

    A269526 refactor.

--- Sloanes/lib/scripts/alec_sequences/a269526.rb
+++ Sloanes/lib/scripts/alec_sequences/a269526.rb
@@ -1,17 +1,15 @@
 require_relative "../helper_sequences/a000217"
+require_relative "../helper_sequences/a002024"
 require_relative "../miscellaneous/a271439"
 
 class OEIS
   def self.nw(n)
-    min_k = (1..Float::INFINITY).find { |i| OEIS.a000217(i - 1) + 2 > n } - 1
-    max_k = (min_k..Float::INFINITY).find { |i| OEIS.a000217(i) > n }
-    return 0 if min_k != max_k
-    n + 2 - 2 * min_k
+    k = a002024(n - 1)
+    a000217(k) <= n ? 0 : n + 2 - 2 * k
   end
 
   def self.sw(n)
-    k = (1..Float::INFINITY).find { |i| OEIS.a000217(i) >= n - 1 }
-    n == OEIS.a000217(k) + 1 ? 0 : n - 1
+    n - 1 == a000217(a002024(n - 1)) ? 0 : n - 1
   end
 
   def self.x_chain(n, f)
@@ -27,12 +25,12 @@ class OEIS
 
   def self.a269526(n)
     (@@a269526.length + 1..n).each do |i|
-      v = ->(x){ OEIS.a271439(x) }
-      h = ->(x){ OEIS.a271439(x - 1) }
-      nw = ->(x){ OEIS.nw(x) }
-      sw = -> (x){ OEIS.sw(x) }
+      v = ->(x){ a271439(x) }
+      h = ->(x){ a271439(x - 1) }
+      nw = ->(x){ nw(x) }
+      sw = ->(x){ sw(x) }
 
-      related_indices = (OEIS.x_chain(i, v) + OEIS.x_chain(i, h) + OEIS.x_chain(i, nw) + OEIS.x_chain(i, sw))
+      related_indices = (x_chain(i, v) + x_chain(i, h) + x_chain(i, nw) + x_chain(i, sw))
       related_terms = related_indices.map { |i| @@a269526[i - 1] }
       @@a269526 << (1..Float::INFINITY).find { |i| !related_terms.include?(i) }
     end
