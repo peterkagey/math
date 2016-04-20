5f4a0a4aac5b96c25d8b4d0b90755947a6d5da79
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-04-19 20:02:32 -0700

    Compute A272011 more lazily.

--- Sloanes/lib/scripts/subsets/a272011.rb
+++ Sloanes/lib/scripts/subsets/a272011.rb
@@ -1,9 +1,24 @@
+require_relative '../helper_sequences/a000788'
 require_relative '../helpers/subset_logic'
 
+class A272011Builder
+  def self.sequence(term, seed)
+    len = seed.length
+    return seed if len > term
+    start = (0..Float::INFINITY).find { |i| OEIS.a000788(i) == len }
+
+    (start..Float::INFINITY).inject(seed) do |accum, i|
+      break seed if seed.length > term
+      seed += Subset.zero_indexed(i + 1).reverse
+    end
+  end
+end
+
 class OEIS
-  @@a272011 = (0..10000).flat_map { |i| Subset.zero_indexed(i).reverse }
+  @@a272011 = []
 
   def self.a272011(n)
+    @@a272011 = A272011Builder.sequence(n, @@a272011)
     @@a272011[n]
   end
 end
--- Sloanes/lib/specs/subsets/a272011_spec.rb
+++ Sloanes/lib/specs/subsets/a272011_spec.rb
@@ -12,31 +12,13 @@ describe OEIS do
     expect(a(2)).to eq 1
     expect(a(3)).to eq 0
     expect(a(4)).to eq 2
-    expect(a(5)).to eq 2
-    expect(a(6)).to eq 0
-    expect(a(7)).to eq 2
-    expect(a(8)).to eq 1
-    expect(a(9)).to eq 2
-    expect(a(10)).to eq 1
-    expect(a(11)).to eq 0
-    expect(a(12)).to eq 3
-    expect(a(13)).to eq 3
-    expect(a(14)).to eq 0
-    expect(a(15)).to eq 3
-    expect(a(16)).to eq 1
-    expect(a(17)).to eq 3
-    expect(a(18)).to eq 1
-    expect(a(19)).to eq 0
-    expect(a(20)).to eq 3
-    expect(a(21)).to eq 2
-    expect(a(22)).to eq 3
-    expect(a(23)).to eq 2
-    expect(a(24)).to eq 0
+
     expect(a(25)).to eq 3
     expect(a(26)).to eq 2
     expect(a(27)).to eq 1
     expect(a(28)).to eq 3
     expect(a(29)).to eq 2
+    expect(a(30)).to eq 1
   end
 
 end
