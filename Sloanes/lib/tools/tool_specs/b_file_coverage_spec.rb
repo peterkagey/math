e52f3a0f7ffe4f78891e0e7c4894d770d6326f13
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-20 09:59:24 -0700

    Separate sequences into categories.

--- Sloanes/lib/tools/tool_specs/b_file_coverage_spec.rb
+++ Sloanes/lib/tools/tool_specs/b_file_coverage_spec.rb
@@ -3,7 +3,15 @@ require_relative '../helpers/sequence_path_iterator'
 describe "OEIS Tests" do
 
   EXPECTED = [
-    "b000002", # a(n) is length of n-th run
+    # Alec Sequences
+    "b269347", # Alec's first sequence
+
+    # EKG
+    "b064413", # EKG sequence
+    "b169849", # EKG Sequence starting at 9.
+    "b256417", # Smoothed EKG sequence
+
+    # Helper sequences
     "b000005", # Number of divisors of n.
     "b000120", # Number of 1s in binary representation of n.
     "b000188", # Square root of largest square dividing n.
@@ -18,10 +26,8 @@ describe "OEIS Tests" do
     "b002522", # n**2 + 1
     "b003056", # n appears n+1 times
     "b003415", # arithmetic derivative of n
-    "b004489", # Base-3 XOR table.
     "b007955", # Product of divisors of n
     "b005117", # Squarefree numbers
-    "b006995", # Binary palindromes.
     "b007913", # Squarefree part of n
     "b032741", # Number of divisors of n
     "b047838", # floor(n^2/2) - 1.
@@ -29,19 +35,26 @@ describe "OEIS Tests" do
     "b054519", # 1 + sum of A000005
     "b057944", # Largest triangular number less than or equal to n
     "b060432", # Partial sums of A002024.
-    "b064413", # EKG sequence
     "b070229", # Next m>n such that m is divisible by lpf(n)
     "b070939", # Length of binary representation of n.
     "b071068", # Number of partitions of n into two squarefree numbers.
+
+    # Miscellaneous
+    "b000002", # a(n) is length of n-th run
+
+    # Palindromes
+    "b006995", # Binary palindromes.
+
+    # Project Euler
     "b080715", # Sums of divisors are prime
     "b085731", # gcd(n, a003415(n))
-    "b169849", # EKG Sequence starting at 9.
-    "b256417", # Smoothed EKG sequence
-    "b269347", # Alec's first sequence
+
+    # Tables
+    "b004489", # Base-3 XOR table.
   ]
 
   it "should check that all b-files are present." do
-    unexpectedly_missing_b_files = BFilePathIterator.missing_b_files - EXPECTED
+    unexpectedly_missing_b_files = BFilePathIterator.new.missing_b_files - EXPECTED
     unexpectedly_missing_b_files.select! { |f| !(f =~ /^b9999..$/)}
     expect(unexpectedly_missing_b_files).to eq []
   end
