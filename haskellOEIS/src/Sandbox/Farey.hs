36fa42f26b1d0c898f1e943f1277e24665580079
Peter Kagey <peterkagey@gmail.com>
2023-06-14 07:08:59 -0700

    Tiny refactor of A338797.

--- haskellOEIS/src/Sandbox/Farey.hs
+++
@@ -1,5 +0,0 @@
-import Helpers.Table (triangleByRows)
-
-import Data.Ratio ((%), denominator)
-farey n = [k % n | k <- [1..n], gcd n k == 1]
-a338797T n k = minimum [denominator $ a + b | a <- farey n, b <- farey k]
--- haskellOEIS/src/Tables/A338797.hs
+++ haskellOEIS/src/Tables/A338797.hs
@@ -1,5 +1,5 @@
 module Tables.A338797 (a338797) where
-import Helpers.Table (tableByAntidiagonals, triangleByRows, indicesByAntidiagonals, triangularize, antidiagonalize)
+import Helpers.Table (triangleByRows)
 import Data.Ratio ((%), denominator)
 
 farey n = [k % n | k <- [1..n], gcd n k == 1]
@@ -8,6 +8,8 @@ a338797T n k = minimum [denominator $ a + b | a <- farey n, b <- farey k]
 
 a338797_row n = map (a338797T n) [1..n]
 
+a338797_list :: [Integer]
 a338797_list = concatMap a338797_row [1..]
 
+a338797 :: Int -> Integer
 a338797 n = a338797_list !! (n - 1)
